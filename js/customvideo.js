f6b0299c12b0e696483c86a563a8820a1027e390
41off <58831292+41off@users.noreply.github.com>
2021-11-22 19:00:26 -0500

    Add files via upload

---
+++ js/customvideo.js
@@ -0,0 +1,26 @@
+﻿(function() {
+    var video = document.getElementById('video')
+        vendorUrl = window.URL || window.webkitURL;
+        
+     navigator.getMedia = navigator.getUserMedia ||
+                          navigator.webkitgetUserMedia ||
+                          navigator.mozgetUserMedia ||
+                          navigator.msgetUserMedia;
+                          
+      // Captue Video
+      navigator.getMedia({
+           video:true,
+           audio: false
+      }, function(stream) {
+          video.src = vendorUrl.createObjectURL(stream);
+          video.play();
+      }, function(error) {
+          // An error ocuured
+          // error.code
+      });
+      
+})();
+      
+           
+     
+                           
